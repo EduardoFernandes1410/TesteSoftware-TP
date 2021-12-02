9883f7101be3247c64d8452ccf32ef0c1f31e868
EduardoFernandes1410 <eduardo.militao1410@gmail.com>
2021-12-02 16:37:05 -0300

    DatabaseController Tests Done

--- communication/database_controller.py
+++ communication/database_controller.py
@@ -29,28 +29,67 @@ class DatabaseController:
         df = pd.DataFrame(columns=['timestamp', 'datestring', 'name', 'quantity', 'price'])
         df.to_pickle(os.path.join(self._database_path, "sales.pkl"))
 
+
+    def check_product_existence_aux(name, table):
+        product = table[table["name"] == name]
+        if(len(product) == 0):
+            return False
+        return True
+
+
+    def check_product_existence(self, name):
+        return check_product_existence_aux(name, self._products)
+
+
+    def get_product_quantity_aux(name, table):
+        if(not DatabaseController.check_product_existence_aux(name, table)):
+            raise Exception("Item not found")
+
+        product = table[table["name"] == name]
+        return product.quantity.values[0]
+
+
+    def get_product_quantity(self, name):
+        return self.get_product_quantity_aux(name, self._products)
+
+
+    def get_product_price_aux(name, table):
+        if(not DatabaseController.check_product_existence_aux(name, table)):
+            raise Exception("Item not found")
+
+        product = table[table["name"] == name]
+        return product.price.values[0]
+
+
+    def get_product_price(self, name):
+        return self.get_product_price_aux(name, self._products)
+
     
-    def validate(self, sale):
+    def validate_sale_aux(sale, table):
         """ checa se as quantidades são possiveis e soma o valor total """
         total_price = 0
-        for name, quantity in self._products.items():
-            if(self._products[self._products["name"] == name].quantity.values[0] < quantity):
+        for name, quantity in sale.items():
+            if(DatabaseController.get_product_quantity_aux(name, table) < quantity):
                 raise Exception("Not enough items in inventory")
 
-            total_price += self._products[self._products["name"] == name].price.values[0]
+            total_price += (DatabaseController.get_product_price_aux(name, table) * quantity)
 
         return total_price
 
 
+    def validate_sale(self, sale):
+        return self.validate(sale, self._products)
+
+
     def insert_new_product(self, name, price, quantity):
-        if(not name in self._products.name.values):
-            self._products = self._products.append({
-                'name': name,
-                'price': price,
-                'quantity': quantity
-            }, ignore_index=True);
-        else:
+        if(self.check_product_existence(name)):
             raise Exception("Product already in database")
+
+        self._products = self._products.append({
+            'name': name,
+            'price': price,
+            'quantity': quantity
+        }, ignore_index=True);
 
 
     def register_new_sale(self, products):
@@ -58,7 +97,7 @@ class DatabaseController:
         datestring = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         for name, quantity in products.items():
-            price = self.read("name == '%s'" % name, "products").price.values[0]
+            price = self.get_product_price(name)
             self._sales = self._sales.append({
                 'timestamp': timestamp,
                 'datestring': datestring,
@@ -77,26 +116,29 @@ class DatabaseController:
 
 
     def update_inventory_price(self, name, new_price):
-        if(name in self._products.name.values):
-            self._products.loc[self._products["name"] == name, 'price'] = new_price
-        else:
+        if(not self.check_product_existence(name)):
             raise Exception("Product not found in database")
+
+        self._products.loc[self._products["name"] == name, 'price'] = new_price
 
 
     def update_inventory_quantity(self, name, delta_quantity):
-        if(name in self._products.name.values):
-            self._products.loc[self._products["name"] == name, 'quantity'] += delta_quantity
-            self._products.loc[self._products["name"] == name, 'quantity'] = max(
-                0,  
-                self._products.loc[self._products["name"] == name, 'quantity'][0]
-                )
-        else:
+        if(not self.check_product_existence(name)):
             raise Exception("Product not found in database")
+
+        self._products.loc[self._products["name"] == name, 'quantity'] += delta_quantity
+        self._products.loc[self._products["name"] == name, 'quantity'] = max(
+            0,  
+            self._products.loc[self._products["name"] == name, 'quantity'][0]
+        )
 
 
     def save_database(self):
         self._products.to_pickle(os.path.join(self._database_path, "products.pkl"))
         self._sales.to_pickle(os.path.join(self._database_path, "sales.pkl"))
+
+
+
 
 # x = DatabaseController('test_db')
 # x.insert_new_product('banana', price=10, quantity=100)
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from view.input_manager import InputManager
 
 input_man = InputManager()
 db_controller = DatabaseController('test_db')
-db_controller.insert_new_product('banana', price=10, quantity=100)
 
 main_controller = MainController(input_manager=input_man,db_controller=db_controller)
 main_controller.run()
