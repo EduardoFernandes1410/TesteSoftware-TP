6d8eca37544c802be996b480a0d38a003c4d4126
Pedro Dias <pedrodias504@gmail.com>
2021-12-03 15:29:13 -0300

    Implementando o método "sales_on_period"

--- controller/report_mode_controller.py
+++ controller/report_mode_controller.py
@@ -1,4 +1,5 @@
 from view.output_manager import OutputManager
+from datetime import datetime
 
 class ReportModeController:
     def __init__(self, input_manager, db_controller):
@@ -7,7 +8,12 @@ class ReportModeController:
 
     def run(self):
         OutputManager.print_report_menu()
-        choice = self._input_man.report_options()
+        try:
+            choice = self._input_man.report_options()
+        except:
+            OutputManager.print_invalid_option()
+            self.run()
+            return
         if choice == "sales_period":
             self.sales_on_period()
         elif choice == "sold":
@@ -24,7 +30,20 @@ class ReportModeController:
             self.run()
 
     def sales_on_period(self):
-        pass
+        OutputManager.print_dataframe(self.sales_on_period_aux())
+
+    def sales_on_period_aux(self):
+        try:
+            OutputManager.print_sales_period("start")
+            start_date = datetime.strptime(self._input_man.report_period_date(), "%Y-%m-%d").timestamp()
+            OutputManager.print_sales_period("end")
+            end_date = datetime.strptime(self._input_man.report_period_date(), "%Y-%m-%d").timestamp()
+        except:
+            OutputManager.print_invalid_option()
+            self.sales_on_period_aux()
+            return
+        sales = self._db_controller.read("timestamp >= {0} and timestamp <= {1}".format(start_date, end_date), "sales")        
+        return sales
 
     def most_sold_items(self, limit=10):
         pass
