ad2602fdee12030d08ab26b20b1a37665985550c
arthurbrito99 <arthur.brito1999@outlook.com>
2021-12-03 17:02:11 -0300

    manager tests - remove item

--- unit_test/test_manager_mode_controller.py
+++ unit_test/test_manager_mode_controller.py
@@ -21,6 +21,21 @@ class TestManagerModeController(TestCase):
         self.db_cont.insert_new_product.side_effect = Exception()
         manager_mode_controller.insert_item()
         mock_out.print_existent_product.assert_called_once()
+        
+    def test_remove_item(self):
+        self.input_man.removing_product.return_value = ('batata doce')
+        manager_mode_controller = ManagerModeController(self.input_man, self.db_cont)
+        manager_mode_controller.remove_item()
+        self.db_cont.remove_product.assert_called_with('batata doce')
+
+    @patch("controller.manager_mode_controller.OutputManager")
+    def test_remove_item_invalid(self, mock_out):
+        self.input_man.removing_product.return_value = ('batata doce')
+        manager_mode_controller = ManagerModeController(self.input_man, self.db_cont)
+        self.db_cont.remove_product.side_effect = Exception()
+        manager_mode_controller.remove_item()
+        mock_out.not_in_inventory_error.assert_called_once()
+        
 
     # def test_insert_item(self):
     #     self.input_man.open_sale_options.return_value = 'exit'
