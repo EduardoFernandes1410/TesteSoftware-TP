c305ed853bba26acea5fdd4506a1f3e46a357b72
augustomaillo <augusto.maillo@gmail.com>
2021-12-08 17:05:36 -0300

    Incluindo testes de integração

--- integration_test/test_integration.py
+++ integration_test/test_integration.py
@@ -1,21 +1,48 @@
 from unittest import TestCase
+from unittest.mock import patch, MagicMock
 import pandas as pd
 import os
 import shutil
 from communication.database_controller import DatabaseController
+from view.input_manager import InputManager
+from controller.cashier_mode_controller import CashierModeController
 
 class IntegrationTest(TestCase):
     def setUp(self):
         self.db_controller = DatabaseController("integration_db")
+        self.input_manager = InputManager()
 
     def tearDown(self):
         shutil.rmtree("integration_db")
 
+    def test_insert_save_and_verify(self):
+        self.db_controller.insert_new_product("batata", 12.90, 12)
+        self.db_controller.save_database()
+
+        self.db_controller = DatabaseController("integration_db")
+        df = self.db_controller.read("name == 'batata'", "products")
+        self.assertEqual(len(df), 1)
 
     def test_insert_product(self):
         self.db_controller.insert_new_product("batata", 12.90, 12)
         df = self.db_controller.read("name == 'batata'", "products")
         self.assertEqual(len(df), 1)
 
+    def test_insert_and_remove_product(self):
+        self.db_controller.insert_new_product("batata", 12.90, 12)
+        self.db_controller.remove_product("batata")
+        df = self.db_controller.read("name == 'batata'", "products")
+        self.assertEqual(len(df), 0)
+
+    @patch("builtins.input")
+    def test_insert_remove_and_sell(self, mock_input):
+        self.db_controller.insert_new_product("batata", 12.90, 12)
+        self.db_controller.remove_product("batata")
+
+        cashier_controller = CashierModeController(self.input_manager, self.db_controller)
+        mock_input.return_value = "batata 1"
+        cashier_controller.add_item()
+        sale = cashier_controller.sale
+        self.assertRaises(Exception, self.db_controller.validate_sale )
 
 
